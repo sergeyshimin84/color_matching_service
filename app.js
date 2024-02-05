1c304d49bb65aea61efd8f5c5293c6cf19afb21a
Sergey Shimin <sergey.shimin84@gmail.com>
2024-02-05 19:42:19 +0700

    Реализован основной функционал

--- app.js
+++ app.js
@@ -1,5 +1,28 @@
 const cols = document.querySelectorAll('.col')
 
+document.addEventListener('keydown', (event) => {
+    event.preventDefault()
+    if (event.code.toLowerCase() === 'space') {
+        setRandomColors()
+    }
+})
+
+document.addEventListener('click', (event) => {
+    const type = event.target.dataset.type
+
+    if (type === 'lock') {
+       const node = 
+        event.target.tagName.toLowerCase() === 'i'
+            ? event.target
+            : event.target.children[0]
+        
+        node.classList.toggle('fa-lock-open')
+        node.classList.toggle('fa-lock')
+    } else if (type === 'copy') {
+        copyToClickboard(event.target.textContent)
+    }
+})
+
 function generateRandomColor () {
     const hexCodes = '0123456789ABCDEF'
     let color = ''
@@ -9,14 +32,32 @@ function generateRandomColor () {
     return '#' + color
 }
 
+function copyToClickboard(text) {
+    return navigator.clipboard.writeText(text)
+}
+
 function setRandomColors() {
     cols.forEach((col) => {
+        const isLocked = col.querySelector('i').classList.contains('fa-lock')
         const text = col.querySelector('h2')
-        const color = generateRandomColor()
+        const button = col.querySelector('button')
+        const color = chroma.random() // генерация цвета с помощью библиотеки chroma (можно не использовать функцию generateRandomColor())
+
+        if (isLocked) {
+            return
+        }
 
         text.textContent = color
         col.style.background = color
+
+        setTextColor(text, color)
+        setTextColor(button, color)
     })
+}
+
+function setTextColor(text, color) {
+    const luminance = chroma(color).luminance()
+    text.style.color = luminance > 0.5 ? 'black' : 'white'
 }
 
 setRandomColors()
